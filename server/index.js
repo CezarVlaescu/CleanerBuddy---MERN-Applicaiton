aa358e4c238faa8e71cf40e3c0f956e99419e5f3
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2024-02-13 18:00:14 +0200

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const getPORT = require('./config/connectionString.json')
-require('./config/db');
-
-const express = require('express');
-const app = express();
-
-const PORT = getPORT.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
